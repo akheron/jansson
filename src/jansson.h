23bc8e468d82bcb52a39af8d070776b972d77227
Petri Lehtinen <petri@digip.org>
2010-12-14 20:57:32 +0200

    Add version info to jansson.h

--- src/jansson.h
+++ src/jansson.h
@@ -16,6 +16,22 @@
 extern "C" {
 #endif
 
+/* version */
+
+#define JANSSON_MAJOR_VERSION  1
+#define JANSSON_MINOR_VERSION  3
+#define JANSSON_MICRO_VERSION  0
+
+/* Micro version is omitted if it's 0 */
+#define JANSSON_VERSION  "1.3"
+
+/* Version as a 3-byte hex number, e.g. 0x010201 == 1.2.1. Use this
+   for numeric comparisons, e.g. #if JANSSON_VERSION_HEX >= ... */
+#define JANSSON_VERSION_HEX  ((JANSSON_MAJOR_VERSION << 16) |   \
+                              (JANSSON_MINOR_VERSION << 8)  |   \
+                              (JANSSON_MICRO_VERSION << 0)))
+
+
 /* types */
 
 typedef enum {
