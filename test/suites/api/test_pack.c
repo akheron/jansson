fa7c2ea070672d05b34b6341142a193e9a7d0a8c
Petri Lehtinen <petri@digip.org>
2011-01-20 21:28:54 +0200

    Update copyright notices for 2011

--- test/suites/api/test_pack.c
+++ test/suites/api/test_pack.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2009, 2010 Petri Lehtinen <petri@digip.org>
- * Copyright (c) 2010 Graeme Smecher <graeme.smecher@mail.mcgill.ca>
+ * Copyright (c) 2009-2011 Petri Lehtinen <petri@digip.org>
+ * Copyright (c) 2010-2011 Graeme Smecher <graeme.smecher@mail.mcgill.ca>
  *
  * Jansson is free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See LICENSE for details.
