6ecba848171f48d0b3806dbc1aa306be8e69bc82
Petri Lehtinen <petri@digip.org>
2011-03-21 08:22:32 +0200

    Fix json_object_size() return value
    Return 0 as documented if the argument is not a JSON object.
    
    Closes GH-18.

--- src/value.c
+++ src/value.c
@@ -91,7 +91,7 @@ size_t json_object_size(const json_t *json)
     json_object_t *object;
 
     if(!json_is_object(json))
-        return -1;
+        return 0;
 
     object = json_to_object(json);
     return object->hashtable.size;
