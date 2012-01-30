0d64e8ef89d0dbfb582a07fb1b02c0d301100d32
Petri Lehtinen <petri@digip.org>
2012-01-30 21:23:15 +0200

    Make test_load.c not depend on the C locale
    Fixes #51.

--- test/suites/api/test_load.c
+++ test/suites/api/test_load.c
@@ -13,13 +13,24 @@ static void file_not_found()
 {
     json_t *json;
     json_error_t error;
+    char *pos;
 
     json = json_load_file("/path/to/nonexistent/file.json", 0, &error);
     if(json)
         fail("json_load_file returned non-NULL for a nonexistent file");
     if(error.line != -1)
         fail("json_load_file returned an invalid line number");
-    if(strcmp(error.text, "unable to open /path/to/nonexistent/file.json: No such file or directory") != 0)
+
+    /* The error message is locale specific, only check the beginning
+       of the error message. */
+
+    pos = strchr(error.text, ':');
+    if(!pos)
+        fail("json_load_file returne an invalid error message");
+
+    *pos = '\0';
+
+    if(strcmp(error.text, "unable to open /path/to/nonexistent/file.json") != 0)
         fail("json_load_file returned an invalid error message");
 }
 
