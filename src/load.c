7fa7949833330ad3dc12e963acf6a42dbedec8be
Rogerz Zhang <rogerz.zhang@gmail.com>
2012-03-21 16:34:14 +0800

    Add json_load_callback()

--- src/load.c
+++ src/load.c
@@ -990,3 +990,24 @@ json_t *json_load_file(const char *path, size_t flags, json_error_t *error)
     fclose(fp);
     return result;
 }
+
+json_t *json_load_callback(json_load_callback_t callback, void *data, size_t flags, json_error_t *error)
+{
+    lex_t lex;
+    json_t *result;
+
+    jsonp_error_init(error, "<callback>");
+
+    if (callback == NULL) {
+        error_set(error, NULL, "wrong arguments");
+        return NULL;
+    }
+
+    if(lex_init(&lex, (get_func)callback, data))
+        return NULL;
+
+    result = parse_json(&lex, flags, error);
+
+    lex_close(&lex);
+    return result;
+}
