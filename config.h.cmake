2f2ba8000e08323531d4bab7a402273fb3b6272a
Paul Harris <paulharris@computer.org>
2012-06-07 23:07:04 +0800

    Fixes for cmake for MSVC2010

--- config.h.cmake
+++ config.h.cmake
@@ -1,7 +1,20 @@
 /* Reduced down to the defines that are actually used in the code */
 
-/* Define to 1 if you have the <inttypes.h> header file. */
+/* Define to 1 if you have the <inttypes.h> (and friends) header file. */
 #cmakedefine HAVE_INTTYPES_H 1
+#cmakedefine HAVE_STDINT_H 1
+#cmakedefine HAVE_SYS_TYPES_H 1
+
+/* We must include this here, as in (eg) utf.h it will want to use
+   the integer type, which in MSVC2010 will be in stdint.h
+   (there is no inttypes.h in MSVC2010) */
+#if defined(HAVE_STDINT_H)
+#  include <stdint.h>
+#elif defined(HAVE_INTTYPES_H)
+#  include <inttypes.h>
+#elif defined(HAVE_SYS_TYPES_H)
+#  include <sys/types.h>
+#endif
 
 /* Define to 1 if you have the <locale.h> header file. */
 #cmakedefine HAVE_LOCALE_H 1
