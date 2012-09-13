ee13c667f10ab6e3ccde9493e71f7ac79496f9e7
Petri Lehtinen <petri@digip.org>
2012-09-13 08:46:14 +0300

    Fix json_real_set() to return -1 on error

--- src/value.c
+++ src/value.c
@@ -751,7 +751,7 @@ double json_real_value(const json_t *json)
 int json_real_set(json_t *json, double value)
 {
     if(!json_is_real(json))
-        return 0;
+        return -1;
 
     json_to_real(json)->value = value;
 
