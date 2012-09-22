3279aacdeed2aac96741c02e2ddecbb5ae087ff8
Petri Lehtinen <petri@digip.org>
2012-09-22 19:22:34 +0300

    jansson 2.4

--- src/jansson.h
+++ src/jansson.h
@@ -21,11 +21,11 @@ extern "C" {
 /* version */
 
 #define JANSSON_MAJOR_VERSION  2
-#define JANSSON_MINOR_VERSION  3
-#define JANSSON_MICRO_VERSION  99
+#define JANSSON_MINOR_VERSION  4
+#define JANSSON_MICRO_VERSION  0
 
 /* Micro version is omitted if it's 0 */
-#define JANSSON_VERSION  "2.4-dev"
+#define JANSSON_VERSION  "2.4"
 
 /* Version as a 3-byte hex number, e.g. 0x010201 == 1.2.1. Use this
    for numeric comparisons, e.g. #if JANSSON_VERSION_HEX >= ... */
