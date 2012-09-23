5804e9de9bc502de955f2add2b4459a51e9d3518
Petri Lehtinen <petri@digip.org>
2012-09-23 13:41:40 +0300

    Set master for 2.5 development mode

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ copyright = u'2009-2012, Petri Lehtinen'
 # built documents.
 #
 # The short X.Y version.
-version = '2.4'
+version = '2.5-dev'
 # The full version, including alpha/beta/rc tags.
 release = version
 
