2489ea90b1ddf4e94bfc898badec785a743b1766
Petri Lehtinen <petri@digip.org>
2013-07-23 13:20:06 +0300

    Update copyrights for 2013

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Jansson'
-copyright = u'2009-2012, Petri Lehtinen'
+copyright = u'2009-2013, Petri Lehtinen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- test/suites/api/test_unpack.c
+++ test/suites/api/test_unpack.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2012 Petri Lehtinen <petri@digip.org>
+ * Copyright (c) 2009-2013 Petri Lehtinen <petri@digip.org>
  * Copyright (c) 2010-2012 Graeme Smecher <graeme.smecher@mail.mcgill.ca>
  *
  * Jansson is free software; you can redistribute it and/or modify
