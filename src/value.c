796090bcf10baf3ef20b415c9c6bff86f36fe070
Erik Olsson <olsson@spotify.com>
2022-07-09 22:33:23 +0200

    Include used header files
    Make source files more self-contained by including the header files they use
    (and remove some they don't).
    This work was aided by the include_what_you_use tool.

--- src/value.c
+++ src/value.c
@@ -14,7 +14,9 @@
 #endif
 
 #include <math.h>
+#include <stdarg.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -24,6 +26,7 @@
 
 #include "hashtable.h"
 #include "jansson.h"
+#include "jansson_config.h"
 #include "jansson_private.h"
 #include "utf.h"
 
