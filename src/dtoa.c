ed06f65412f5a3dc3391197cf6d72aacac468160
Petri Lehtinen <petri@digip.org>
2024-03-18 22:32:26 +0200

    Rename a symbol to avoid collision on Windows

--- src/dtoa.c
+++ src/dtoa.c
@@ -3487,7 +3487,7 @@ retlow1:
 #endif /* NO_STRTOD_BIGCOMP */
 
  double
-strtod(const char *s00, char **se)
+strtod__unused(const char *s00, char **se)
 {
 	int bb2, bb5, bbe, bd2, bd5, bbbits, bs2, c, e, e1;
 	int esign, i, j, k, nd, nd0, nf, nz, nz0, nz1, sign;
