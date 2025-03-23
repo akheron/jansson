05a10aa8af8dcd832dd3270ef7a89bf5a3b834a1
Petri Lehtinen <petri@digip.org>
2025-03-23 22:17:53 +0200

    Fix code formatting

--- src/dump.c
+++ src/dump.c
@@ -26,7 +26,7 @@
 #define MAX_INTEGER_STR_LENGTH 25
 #define MAX_REAL_STR_LENGTH    25
 
-#define FLAGS_TO_INDENT(f)    ((f)&0x1F)
+#define FLAGS_TO_INDENT(f)    ((f) & 0x1F)
 #define FLAGS_TO_PRECISION(f) (((f) >> 11) & 0x1F)
 
 struct buffer {
